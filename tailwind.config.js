306cd818248797bd67ed7aa4335d099def1c0989
Gabriel-P-Freitas <f.pessoa@escolar.ifrn.edu.br>
2024-11-22 23:45:01 -0300

    iniciando main + ajustando header e global class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,31 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '578px',
+      // => @media (min-width: 578px) { ... }
+
+      'lg': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1rem',
+        lg: '2rem',
+        xl: '9.125rem',
+        '2xl': '9.125rem',
+      },
+
     extend: {},
   },
   plugins: [],
 }
-
+}
